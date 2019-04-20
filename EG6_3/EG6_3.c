c781eeecc7cc29961590646d900545b4e7c00d82
cssxsh <cssxsh@gmail.com>
2019-04-20 16:20:03 +0800

    2019/04/20

---
+++ EG6_3/EG6_3.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+const char OP[4] = {'+', '-', '*', '/'};
+int CalcTest(int a, int b, int op);
+int main(void)
+{
+	int grade = 0, errnum = 0;
+	int a, b, op;
+	srand(1701420218);
+
+	for (int i = 0; i < 10; i++)
+	{
+		a = rand() % 10 + 1;
+		op = OP[rand() % 4];
+		b = rand() % 10 + 1;
+		printf("%d %c %d = ", a, op ,b);
+		if (CalcTest(a, b, op))
+		{
+			printf("Right!\n");
+			grade += 10;
+		}
+		else
+		{
+			printf("Not correct!\n");
+			errnum += 1;
+		}
+	}
+	printf("Grade: %d, ErrNum: %d", grade, errnum);
+
+	return 0;
+}
+int CalcTest(int a, int b, int op)
+{
+	int result;
+	scanf("%d", &result);
+	switch (op)
+	{
+	case '+':
+		return (a + b == result);
+		break;
+	case '-':
+		return (a - b == result);
+		break;
+	case '*':
+		return (a * b == result);
+		break;
+	case '/':
+		return (a / b == result);
+		break;
+	default:
+		break;
+	}
+	return 0;
+}
---
+++ EG7_1/EG7_1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+// 参数应该是数组或者指针形式
+void mystrc(char *s, char *t);
+int main(void)
+{
+	char s[80], t[80];
+	gets(s);
+	gets(t);
+	mystrc(s, t);
+	puts(t);
+	return 0;
+}
+void mystrc(char *s, char *t)//; 多余的分号
+{
+	while (*t != '\0') t++;
+	while (*s != '\0')
+	{
+		*t = *s;
+		t++;
+		s++;
+	}
+	*t = '\0';
+}
