69a8db0fd05b8941a7f8586beafdacd3e42a106a
cssxsh <cssxsh@gmail.com>
2019-04-20 15:56:37 +0800

    2019/04/20

--- EG5_3/EG5_3.c
+++ EG5_3/EG5_3.c
@@ -4,12 +4,12 @@
 
 int main(void)
 {
-	int n = 5;
+	int n = 5, sum = 0, temp1 = 0, temp2 = 0;
 	int **arr = NULL;
-	int sum = 0;
 
-	printf("请输入魔方的大小：");
+	printf("请输入矩阵的大小：\n");
 	scanf("%d", &n);
+	printf("请输入矩阵：\n");
 	arr = (int **)malloc(n * sizeof(int *));
 	for (int i = 0; i < n; i++)
 	{
@@ -23,6 +23,42 @@ int main(void)
 	for (int i = 0; i < n; i++)
 	{
 		sum += arr[i][i];
+		temp1 += arr[i][n - 1];
+	}
+	if (sum != temp1)
+	{
+		printf("这个矩阵不是魔方！\n");
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			temp1 = temp2 = 0;
+			for (int j = 0; j < n; j++)
+			{
+				temp1 += arr[i][j];
+				temp2 += arr[j][i];
+			}
+			if (sum != temp1 || sum != temp2)
+			{
+				printf("这个矩阵不是魔方！\n");
+				break;
+			}
+		}
+		if (sum == temp1 && sum == temp2)
+		{
+			printf("这个矩阵是魔方！\n");
+		}
+	}
+
+	printf("矩阵结构如下：\n");
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%d\t", arr[i][j]);
+		}
+		printf("\n");
 	}
 
 
@@ -33,3 +69,13 @@ int main(void)
 	free(arr);
 	return 0;
 }
+
+
+/*
+17 24 1 8 15
+23 5 7 14 16
+4 6 13 20 22
+10 12 19 21 3
+11 18 25 2 9
+
+*/
--- EG6_2_2/EG6_2_2.c
+++ EG6_2_2/EG6_2_2.c
@@ -1,7 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
+int AddTest(int a, int b);
 int main(void)
 {
+	int grade = 0, errnum = 0;
+	int a, b;
+	srand(1701420218);
+
+	for (int i = 0; i < 10; i++)
+	{
+		a = rand() % 10 + 1;
+		b = rand() % 10 + 1;
+		printf("%d + %d = ", a, b);
+		if (AddTest(a, b))
+		{
+			printf("Right!\n");
+			grade += 10;
+		}
+		else
+		{
+			printf("Not correct!\n");
+			errnum += 1;
+		}
+	}
+	printf("Grade: %d, ErrNum: %d", grade, errnum);
+
 	return 0;
+}
+int AddTest(int a, int b)
+{
+	int sum;
+	scanf("%d", &sum);
+	return (a + b == sum);
 }
